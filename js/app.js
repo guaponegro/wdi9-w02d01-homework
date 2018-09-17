eb87143e2038bf2340bf9866da6d14a740205225
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-17 16:55:14 -0600

    Part 1 of Data Structure hw completed

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// 1. Data Types
+
+// Light Switch- Boolean
+if(x > 5){
+    console.log("True");
+} else{
+    console.log("False");
+}
+
+// Email Address- String
+"stevenariol@gmail.com"
+
+// Spaceship- Object
+const spaceship = {
+    weapons: ["laser blasters", "tractor beam"],
+    engine: "warp drive",
+    storage: "hull",
+};
+
+// List of Student Names- Array
+const studentList = ["James", "Karen", "Jake", "Jessie"];
+
+// List of Student Names with a location- Object
+
+const studentList = {
+    [name: "James", location: "Denver"], 
+    [name: "Karen", location: "Los Angeles"]
+}
+
+// List of Student Names with location and 
+// favorite tv shows- Object
+
+const studentList = {
+    [name: "James", location: "Denver", favTvShow: "Lost"],
+    [name: "Karen", location: "Los Angeles", favTvShow: "House of Cards"];
+}
