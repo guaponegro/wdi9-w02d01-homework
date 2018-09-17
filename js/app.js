23f4dce32a4c109200cbf6ca049253e6c5658fe5
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-17 17:12:33 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -1,36 +1,49 @@
-// 1. Data Types
+// // 1. Data Types
 
-// Light Switch- Boolean
-if(x > 5){
-    console.log("True");
-} else{
-    console.log("False");
+// // Light Switch- Boolean
+// if(x > 5){
+//     console.log("True");
+// } else{
+//     console.log("False");
+// }
+
+// // Email Address- String
+// "stevenariol@gmail.com"
+
+// // Spaceship- Object
+// const spaceship = {
+//     weapons: ["laser blasters", "tractor beam"],
+//     engine: "warp drive",
+//     storage: "hull",
+// };
+
+// // List of Student Names- Array
+// const studentList = ["James", "Karen", "Jake", "Jessie"];
+
+// // List of Student Names with a location- Object
+
+// const studentList = {
+//     [name: "James", location: "Denver"], 
+//     [name: "Karen", location: "Los Angeles"]
+// }
+
+// // List of Student Names with location and 
+// // favorite tv shows- Object
+
+// const studentList = {
+//     [name: "James", location: "Denver", favTvShow: "Lost"],
+//     [name: "Karen", location: "Los Angeles", favTvShow: "House of Cards"];
+// }
+
+// 2. Take it Easy
+const rainbowColors = ["red", "orange", "yellow", "green", "blue", "purple", "violet"];
+console.log(rainbowColors[4]);
+
+const steven = {
+    favoriteFood: "Italian",
+    hobby: "watching sports",
+    town: "Denver",
+    favoriteDataType: "boolean",
+
 }
-
-// Email Address- String
-"stevenariol@gmail.com"
-
-// Spaceship- Object
-const spaceship = {
-    weapons: ["laser blasters", "tractor beam"],
-    engine: "warp drive",
-    storage: "hull",
-};
-
-// List of Student Names- Array
-const studentList = ["James", "Karen", "Jake", "Jessie"];
-
-// List of Student Names with a location- Object
-
-const studentList = {
-    [name: "James", location: "Denver"], 
-    [name: "Karen", location: "Los Angeles"]
-}
-
-// List of Student Names with location and 
-// favorite tv shows- Object
-
-const studentList = {
-    [name: "James", location: "Denver", favTvShow: "Lost"],
-    [name: "Karen", location: "Los Angeles", favTvShow: "House of Cards"];
-}
+console.log(steven.hobby);
